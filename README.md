8b96f290b7fb53216c3836dc65a7794b9f24e5cb
Ryan Groch <ryan.w.groch@gmail.com>
2024-01-13 13:57:34 -0600

    docs/style: Rewrites readme and applies prettier formatting

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
-# Tauri + Vanilla TS
+# ComEd Prices App
 
-This template should help get you started developing with Tauri in vanilla HTML, CSS and Typescript.
+This is a simple desktop application which retrieves and displays electricity prices from [ComEd’s price data API](https://hourlypricing.comed.com/hp-api/). I created this app in order to ensure that I know the most cost-effective times to turn on larger appliances.
 
-## Recommended IDE Setup
+This app uses the [Tauri framework](https://tauri.app/) and I initialized it with `create-tauri-app`. Most of the code is boilerplate that I left unedited. Notable files include the following:
 
-- [VS Code](https://code.visualstudio.com/) + [Tauri](https://marketplace.visualstudio.com/items?itemName=tauri-apps.tauri-vscode) + [rust-analyzer](https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer)
+- `src/main.ts` - Includes the core logic of the app, including calculations and API calls.
+- `src/styles.css` - Specifies the stylistic design of the UI.
+- `index.html` - Specifies the content of the UI.
+- `.github/workflows/main.yml` - This file was taken from the [Tauri Action GitHub repository](https://github.com/tauri-apps/tauri-action), and I’ve mostly left it untouched. It ensures that GitHub automatically creates new binaries for each release to ensure cross-platform compatibility.
+
+This app is not affiliated with ComEd in any way.
--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,33 @@
-import z from "zod"
-import { getClient } from '@tauri-apps/api/http';
+import z from "zod";
+import { getClient } from "@tauri-apps/api/http";
 
-const schema = z.object({
-  millisUTC: z.string(),
-  price: z.string()
-}).array().nonempty();
+const schema = z
+  .object({
+    millisUTC: z.string(),
+    price: z.string(),
+  })
+  .array()
+  .nonempty();
 
-const inPrevHour = (now: Date, check: Date) => 
-    (now.getHours() === 0 
-      ? check.getHours() === 23 
-      : check.getHours() === now.getHours() - 1);
+const inPrevHour = (now: Date, check: Date) =>
+  now.getHours() === 0
+    ? check.getHours() === 23
+    : check.getHours() === now.getHours() - 1;
 
-const inCurrOrPrevHour = (now: Date, check: Date) => 
-    // Valid date
-    !isNaN(+check)
-    // Hours are the same
-    && ((now.getHours() === check.getHours())
-        // Hours are different and minutes aren't 0
-        || inPrevHour(now, check));
+const inCurrOrPrevHour = (now: Date, check: Date) =>
+  // Valid date
+  !isNaN(+check) &&
+  // Check if current hour
+  (now.getHours() === check.getHours() ||
+    // Check if prev hour
+    inPrevHour(now, check));
 
-const oneMinute = 60*1000;
+const oneMinute = 60 * 1000;
 const fiveMinutes = 5 * oneMinute;
 
-const currHourUrl = "https://hourlypricing.comed.com/api?type=currenthouraverage";
+const currHourUrl =
+  "https://hourlypricing.comed.com/api?type=currenthouraverage";
+
 const fiveMinutesUrl = "https://hourlypricing.comed.com/api?type=5minutefeed";
 
 window.addEventListener("DOMContentLoaded", () => {
@@ -35,26 +40,39 @@ window.addEventListener("DOMContentLoaded", () => {
 
   const btnElem = document.querySelector("#btn");
 
-  if (!fiveMinutesElem || !currHourElem || !prevHourElem 
-      || !timestampedElem || !fetchedElem || !btnElem) 
+  if (
+    !fiveMinutesElem ||
+    !currHourElem ||
+    !prevHourElem ||
+    !timestampedElem ||
+    !fetchedElem ||
+    !btnElem
+  )
     return;
-    
+
   let lastFetched: Date | null = null;
 
   const setError = () => {
-    fiveMinutesElem.textContent = prevHourElem.textContent = currHourElem.textContent = "Error";
-  }
-  
+    fiveMinutesElem.textContent =
+      prevHourElem.textContent =
+      currHourElem.textContent =
+        "Error";
+  };
+
   const fetchData = async () => {
     const client = await getClient();
-    const currHourFetch = schema.safeParse((await client.get(currHourUrl)).data);
-    const fiveMinuteFetch = schema.safeParse((await client.get(fiveMinutesUrl)).data);
+    const currHourFetch = schema.safeParse(
+      (await client.get(currHourUrl)).data
+    );
+    const fiveMinuteFetch = schema.safeParse(
+      (await client.get(fiveMinutesUrl)).data
+    );
 
     if (!currHourFetch.success || !fiveMinuteFetch.success) {
       setError();
       return;
     }
-    
+
     // Set curr hour
     const currHourVal = Number(currHourFetch.data[0].price);
     currHourElem.textContent = `${currHourVal.toFixed(1)}¢`;
@@ -72,10 +90,8 @@ window.addEventListener("DOMContentLoaded", () => {
 
     for (const snapshot of fiveMinuteFetch.data) {
       indexDate.setTime(Number(snapshot.millisUTC));
-      if (!inCurrOrPrevHour(lastTimestamp, indexDate)) {
-        break;
-      }
-      
+      if (!inCurrOrPrevHour(lastTimestamp, indexDate)) break;
+
       if (inPrevHour(lastTimestamp, indexDate)) {
         total += Number(snapshot.price);
         valsUsed++;
@@ -85,19 +101,21 @@ window.addEventListener("DOMContentLoaded", () => {
     const avg = total / valsUsed;
     prevHourElem.textContent = `${avg.toFixed(1)}¢`;
 
-    // Update timestamps fetched and timestamp
+    // Update timestamps
     lastFetched = new Date();
     timestampedElem.textContent = lastTimestamp.toLocaleTimeString();
     fetchedElem.textContent = lastFetched.toLocaleTimeString();
-  }
+  };
 
   const fetchDataAuto = () => {
-    if (lastFetched !== null && 
-      new Date().getTime() - lastFetched?.getTime() < fiveMinutes) 
+    if (
+      lastFetched !== null &&
+      new Date().getTime() - lastFetched.getTime() < fiveMinutes
+    )
       return;
-    
+
     fetchData();
-  }
+  };
 
   btnElem.addEventListener("click", fetchData);
   setInterval(fetchDataAuto, oneMinute);
